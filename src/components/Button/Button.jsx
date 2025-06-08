4e602242f331b3aac29071a6ffc8ac88efb19fdb
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-09 01:44:09 +0400

    Refactor Button component, improve styling, and add Header component
    - Updated Button component to include `isActive` and `isDisabled` props with new styles.
    - Replaced `isPrimary` logic and improved CSS using variables for flexibility.
    - Added a reusable Header component with associated styles and integrated it into MainLayout.
    - Enhanced global CSS with additional color variables for buttons.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import cls from './Button.module.css';
 
-const isPrimary = true;
-
-export const Button = ({ onClick, children }) => {
+export const Button = ({ onClick, isActive, isDisabled, children }) => {
   return (
-    // <button className={isPrimary ? cls.primary : cls.btn}>Кнопка</button>
-    <button className={`${cls.btn} ${isPrimary ? cls.primary : ''}`} onClick={onClick}>
+    <button className={`${cls.btn} ${isActive ? cls.active : ''}`} onClick={onClick} disabled={isDisabled}>
       {children}
     </button>
   );
