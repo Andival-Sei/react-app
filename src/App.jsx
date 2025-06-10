ac2742ed59be9518983aaade97e780354b49f9ff
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-10 12:49:35 +0400

    Update HomePage and QuestionCard functionality with routing improvements
    - Integrated mapped `cards` data in `HomePage` for dynamic rendering of `QuestionCard` components.
    - Expanded App routes to include `NotFoundPage` and a placeholder route for question details.
    - Enhanced `QuestionCard` with props support for dynamic data and navigation integration.
    - Added `NotFoundPage` component with corresponding styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './components/MainLayout/index.jsx';
-import HomePage from './pages/HomePage/HomePage.jsx';
+import { HomePage } from './pages/HomePage/HomePage.jsx';
+import { NotFoundPage } from './pages/NotFoundPage/index.jsx';
 
 function App() {
   // return <MainLayout />;
@@ -11,6 +12,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<h1>forbidden !!!</h1>} />
           <Route path="/addquestion" element={<h1>add question</h1>} />
+          <Route path="/question/:id" element={<h1>question page</h1>} />
+
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
-import QuestionCard from '../../components/QuestionCard/QuestionCard.jsx';
+import { QuestionCard } from '../../components/QuestionCard/index.jsx';
+
+const cards = [
+  {
+    id: 1,
+    question: 'Что такое React?',
+    answer: 'React - это библиотека для создания пользовательских интерфейсов.',
+    description:
+      'Reac - это JavaScript-библиотека, разработтная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.',
+    resources: ['https://react.dev', 'https://react.dev/reference/react'],
+    level: 1,
+    completed: false,
+    editDate: '03.02.2025, 19:49',
+  },
+  {
+    id: 1,
+    question: 'Что такое React111?',
+    answer: 'React - это библиотека для создания пользовательских интерфейсов.',
+    description:
+      'Reac - это JavaScript-библиотека, разработтная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.',
+    resources: ['https://react.dev', 'https://react.dev/reference/react'],
+    level: 1,
+    completed: false,
+    editDate: '03.02.2025, 19:49',
+  },
+  {
+    id: 1,
+    question: 'Что такое React?',
+    answer: 'React - это библиотека для создания пользовательских интерфейсов.',
+    description:
+      'Reac - это JavaScript-библиотека, разработтная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.',
+    resources: ['https://react.dev', 'https://react.dev/reference/react'],
+    level: 1,
+    completed: false,
+    editDate: '03.02.2025, 19:49',
+  },
+];
 
 export const HomePage = () => {
   return (
-    <div>
-      <QuestionCard />
-    </div>
+    <>
+      {cards.map((card, index) => {
+        return <QuestionCard card={card} key={index} />;
+      })}
+    </>
   );
 };
