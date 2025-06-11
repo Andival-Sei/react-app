c5690ffe9602b6789bfc1211fa274a6cac806ded
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-11 21:15:35 +0400

    Добавлена страница AddQuestionPage и интеграция в маршруты
    - Реализован новый компонент `AddQuestionPage` для добавления вопросов.
    - Добавлены маршруты в `App.jsx` для отображения страницы добавления вопросов.
    - Созданы стили `AddQuestionPage.module.css` для оформления страницы.
    - Добавлена новая переменная `--green-color` в `index.css` для акцентного цвета и использования в стилях.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { MainLayout } from './components/MainLayout/index.jsx';
 import { HomePage } from './pages/HomePage/HomePage.jsx';
 import { NotFoundPage } from './pages/NotFoundPage/index.jsx';
 import { QuestionPage } from './pages/QuestionPage/index.jsx';
+import { AddQuestionPage } from './pages/AddQuestionPage/index.jsx';
 
 function App() {
   // return <MainLayout />;
@@ -12,7 +13,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<h1>forbidden !!!</h1>} />
-          <Route path="/addquestion" element={<h1>add question</h1>} />
+          <Route path="/addquestion" element={<AddQuestionPage />} />
           <Route path="/question/:id" element={<QuestionPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
