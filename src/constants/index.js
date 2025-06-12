79db01f5893cac9642bf9a982b8257491cd67f2e
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-13 03:03:13 +0400

    Обновлены настройки среды и перенаправления API
    - Добавлен файл `vercel.json` для настройки перенаправлений API в продакшене.
    - Перемещена константа `API_URL` в зависимости от режима окружения.
    - Исправлен путь импорта `AuthContext` в `AuthProvider`.
    - Добавлена папка `.vercel` в `.gitignore`.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-export const API_URL = 'http://localhost:3001';
+// export const API_URL = 'http://localhost:3001';
+
+export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3001';
 
 export const AUTH_STORAGE = 'reactCardLogin';
 export const THEME_STORAGE = 'reactCardTheme';
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../auth/AuthProvider/imdex.jsx';
+import { AuthContext } from '../auth/AuthProvider/index.jsx';
 
 export const useAuth = () => {
   return useContext(AuthContext);
